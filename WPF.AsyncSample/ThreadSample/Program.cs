0396807990eada94028070a8d6bf7579c229e62f
JeremyWu <Jeremy_Wu@xus-wx.com>
2020-07-08 14:32:57 +0800

    Thread join learning

---
+++ WPF.AsyncSample/ThreadSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread th1 = new Thread(() => {
+                Thread.Sleep(500);
+                Console.WriteLine("线程1执行完毕！");
+            });
+            th1.Start();
+            Thread th2 = new Thread(() => {
+                Thread.Sleep(1000);
+                Console.WriteLine("线程2执行完毕！");
+            });
+            th2.Start();
+            //阻塞主线程
+            th1.Join();
+            th2.Join();
+            Console.WriteLine("主线程执行完毕！");
+            Console.ReadKey();
+        }
+    }
+}
