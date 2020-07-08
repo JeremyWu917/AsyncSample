48d2e5c2dd0aeb282e5df2f856b748839681b236
JeremyWu <Jeremy_Wu@xus-wx.com>
2020-07-08 14:08:12 +0800

    Add Task Learning

---
+++ WPF.AsyncSample/Task.Sample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.new方式实例化一个Task，需要通过Start方法启动
+            Task task = new Task(() =>
+            {
+                Thread.Sleep(100);
+                Console.WriteLine($"hello, task1的线程ID为{Thread.CurrentThread.ManagedThreadId}");
+            });
+            task.Start();
+
+            //2.Task.Factory.StartNew(Action action)创建和启动一个Task
+            Task task2 = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(100);
+                Console.WriteLine($"hello, task2的线程ID为{ Thread.CurrentThread.ManagedThreadId}");
+            });
+
+            //3.Task.Run(Action action)将任务放在线程池队列，返回并启动一个Task
+            Task task3 = Task.Run(() =>
+            {
+                Thread.Sleep(100);
+                Console.WriteLine($"hello, task3的线程ID为{ Thread.CurrentThread.ManagedThreadId}");
+            });
+            Console.WriteLine("执行主线程！");
+            Console.ReadKey();
+
+
+
+
+            //执行结果为：
+            //执行主线程！
+            //hello, task1的线程ID为3
+            //hello, task3的线程ID为6
+            //hello, task2的线程ID为4
+
+            //结论：先打印"执行主线程"，然后再打印各个任务，说明了Task不会阻塞主线程。
+        }
+    }
+}
