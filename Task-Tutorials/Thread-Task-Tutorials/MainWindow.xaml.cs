fac1184205722676bb2c437d511f5b5e96d28ac4
JeremyWu917 <1280442096@qq.com>
2022-11-13 21:49:41 +0800

    core: add CancellationTokenSource to cancel thread

--- Task-Tutorials/Thread-Task-Tutorials/MainWindow.xaml.cs
+++ Task-Tutorials/Thread-Task-Tutorials/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -43,42 +44,126 @@ namespace Thread_Task_Tutorials
             // 注意：多线程内部发生异常后，try-catch 无法步骤异常
             // 若想捕捉到异常需要批量等待
 
-            try
-            {
-                List<Task> tasks = new();
-                string k = string.Empty;
-                for (int i = 0; i < 10; i++)
-                {
-                    k = $"TaskAdvanced_Click_{i}";
-                    tasks.Add(Task.Run(() =>
-                    {
-                        if (k.Equals("TaskAdvanced_Click_2"))
-                        {
-                            throw new Exception("TaskAdvanced_Click_2 异常");
-                        }
-                        else if (k.Equals("TaskAdvanced_Click_5"))
-                        {
-                            throw new Exception("TaskAdvanced_Click_5 异常");
-                        }
-                        else if (k.Equals("TaskAdvanced_Click_8"))
-                        {
-                            throw new Exception("TaskAdvanced_Click_8 异常");
-                        }
-                    }));
-                    Task.WaitAll(tasks.ToArray());
-                }
-            }
-            catch (AggregateException ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
-            }
+            //try
+            //{
+            //    List<Task> tasks = new();
+            //    //string k = string.Empty;
+            //    for (int i = 0; i < 110; i++)
+            //    {
+            //        string k = $"TaskAdvanced_Click_{i}";
+            //        tasks.Add(Task.Run(() =>
+            //        {
+            //            if (k.Equals("TaskAdvanced_Click_2"))
+            //            {
+            //                throw new Exception("TaskAdvanced_Click_2 异常");
+            //            }
+            //            else if (k.Equals("TaskAdvanced_Click_5"))
+            //            {
+            //                throw new Exception("TaskAdvanced_Click_5 异常");
+            //            }
+            //            else if (k.Equals("TaskAdvanced_Click_8"))
+            //            {
+            //                throw new Exception("TaskAdvanced_Click_8 异常");
+            //            }
+            //        }));
+            //        Task.WaitAll(tasks.ToArray());
+            //    }
+            //}
+            //catch (AggregateException ex)
+            //{
+            //    Debug.WriteLine(ex.Message);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Debug.WriteLine(ex.Message);
+            //}
 
 
             // 3、线程取消
+            // 线程抛出异常时就不再继续往下执行了，其实就取消了
+            // 方式一、自定义一个变量
+
+            //bool isOk = true;
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    string k = $"Exception_{i}";
+            //    Task.Run(() =>
+            //    {
+            //        if (!isOk)
+            //        {
+            //            throw new Exception($"线程异常取消了-{k}");
+            //        }
+            //        else
+            //        {
+            //            Debug.WriteLine($"线程正常开始了-{k}");
+            //        }
+
+            //        try
+            //        {
+            //            if (k.Equals("Exception_6"))
+            //            {
+            //                //isOk = false;
+            //                throw new Exception("异常发生了");
+            //            }
+            //            else if (k.Equals("Exception_15"))
+            //            {
+            //                //isOk = false;
+            //                throw new Exception("异常发生了");
+            //            }
+            //        }
+            //        catch (Exception)
+            //        {
+            //            isOk = false;
+            //        }
+            //    });
+
+            //}
+
+            // 方式二、采用线程安全的做法 CancellationTokenSource 实例 IsCancellationRequested 属性和 Cancel 方法
+
+            CancellationTokenSource cts = new();
+            //_ = cts.IsCancellationRequested;
+            for (int i = 0; i < 200; i++)
+            {
+                string k = $"Exception_{i}";
+                Task.Run(() =>
+                {
+                    if (cts.IsCancellationRequested)
+                    {
+                        throw new Exception($"线程Id-{Environment.CurrentManagedThreadId}异常取消了-{k}");
+                    }
+                    else
+                    {
+                        Debug.WriteLine($"线程正常开始了-{k}");
+                    }
+
+                    try
+                    {
+                        if (k.Equals("Exception_6"))
+                        {
+                            throw new Exception("异常发生了");
+                        }
+                        else if (k.Equals("Exception_15"))
+                        {
+                            throw new Exception("异常发生了");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        cts.Cancel();
+                    }
+
+                    if (cts.IsCancellationRequested)
+                    {
+                        throw new Exception($"线程Id-{Environment.CurrentManagedThreadId}异常取消了-{k}");
+                    }
+                    else
+                    {
+                        Debug.WriteLine($"线程正常结束了-{k}");
+                    }
+                }, cts.Token);
+
+            }
 
             // 4、临时变量问题
 
